85f4398513418a314f1fb05836c1858ecdfb0cd7
Dmitriy Nefedchenko <dnefedchenko@gmail.com>
2016-08-28 17:56:50 +0300

    Test cases extended

--- test/com/freeman/calculator/CalculatorTest.java
+++ test/com/freeman/calculator/CalculatorTest.java
@@ -4,10 +4,18 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static com.freeman.calculator.KeyAction.PLUS;
+import static com.freeman.calculator.KeyAction.MINUS;
+import static com.freeman.calculator.KeyAction.MULTIPLY;
+import static com.freeman.calculator.KeyAction.DIVIDE;
+
 /**
  * Created by freeman on 07.08.2016.
  */
 public class CalculatorTest {
+    private static String EXPRESSION = "3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2";
+
     private Calculator testee;
 
     @Before
@@ -17,7 +25,26 @@ public class CalculatorTest {
 
     @Test
     public void testAdding() {
-//        Assert.assertEquals("3.5", testee.calculate("3+4*2/(1-5)^2"));
-        Assert.assertEquals("36", testee.calculate("( − 1 − 5 ) ^ 2"));
+        assertEquals("One plus two equals three", "3.0", testee.calculate("1 " + PLUS.action + " 2"));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals("Three minus one equals two", "2.0", testee.calculate("3 " + MINUS.action + " 1"));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals("Three multiplied by two equals six", "6.0", testee.calculate("3 " + MULTIPLY.action + " 2"));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals("Six divided by two equals three", "3.0", testee.calculate("6 " + DIVIDE.action + " 2"));
+    }
+
+    @Test
+    public void testExpression() {
+        assertEquals("Evaluated expression equals 3.5", "3.5", testee.calculate(EXPRESSION));
     }
 }
